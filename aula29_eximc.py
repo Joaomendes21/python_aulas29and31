c6a7ddbb767e76258db08acfd289316553f20d6b
João Vítor mendes <88053372+Joaomendes21@users.noreply.github.com>
2025-06-11 13:08:20 -0300

    Add files via upload

---
+++ aula29_eximc.py
@@ -0,0 +1,6 @@
+nome = 'joao mendes'
+altura = 1.70
+peso = 82
+imc = peso / (altura * altura)
+print( nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos e seu imc é', imc)
---
+++ aula31_fstring.py
@@ -0,0 +1,9 @@
+nome = 'joao mendes'
+altura = 1.70
+peso = 82
+imc = peso / (altura * altura)
+linha1 = f'{nome} tem {altura:.2f} de altura,'
+linha2 = f'pesa {peso} quilos e seu imc é {imc:.2f}'
+
+print(linha1)
+print(linha2)
